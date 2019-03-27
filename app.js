aa3c344b9a68d1d576aaa5e5befd766ad6115f4d
sikandre <sik.andre@gmail.com>
2019-03-27 16:46:10 +0000

    verify token updated

--- app.js
+++ app.js
@@ -59,7 +59,7 @@ app.post('/webhook', (req, res) => {
 app.get('/webhook', (req, res) => {
   
   /** UPDATE YOUR VERIFY TOKEN **/
-  const VERIFY_TOKEN = "<YOUR_VERIFY_TOKEN>";
+  const VERIFY_TOKEN = "ravenloft";
   
   // Parse params from the webhook verification request
   let mode = req.query['hub.mode'];
