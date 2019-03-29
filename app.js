dd9aea3d72ca23a1cc3f82f66fbfc11622be3215
sikandre <sik.andre@gmail.com>
2019-03-29 14:49:01 +0000

    fix1

--- app.js
+++ app.js
@@ -276,6 +276,30 @@ function handleAttachments(sender_psid, messageAttch) {
     callSendAPI(sender_psid, response);
 }
 
+function callSendAPI(sender_psid, response) {
+    // Construct the message body
+    let request_body = {
+      "recipient": {
+        "id": sender_psid
+      },
+      "message": response
+    }
+  
+    // Send the HTTP request to the Messenger Platform
+    request({
+      "uri": "https://graph.facebook.com/v2.6/me/messages",
+      "qs": { "access_token": PAGE_ACCESS_TOKEN },
+      "method": "POST",
+      "json": request_body
+    }, (err, res, body) => {
+      if (!err) {
+        console.log('message sent!')
+      } else {
+        console.error("Unable to send message:" + err);
+      }
+    });
+}
+
 /*
  * Call the Send API. The message data goes in the body. If successful, we'll
  * get the message id in a response
