2b0e68181adf3b988a6cd123630612c2604e7c65
sikandre <sik.andre@gmail.com>
2019-03-29 12:01:59 +0000

    fix4

--- app.js
+++ app.js
@@ -120,17 +120,42 @@ function receivedMessage(event) {
     console.log(JSON.stringify(message));
 
     var messageText = message.text;
-    var messagePostback = event.message.postback;
+    var messageAttachments = message.attachment;
 
     if (messageText) {
         switch (messageText) {
             default:
                 sendTextMessage(senderID, messageText);
         }
+    } else if (messageAttachments){
+        let attachment_url = received_message.attachments[0].payload.url;
+        response = {
+            "attachment": {
+              "type": "template",
+                "payload": {
+                    "template_type": "generic",
+                    "elements": [{
+                        "title": "Is this the right picture?",
+                        "subtitle": "Tap a button to answer.",
+                        "image_url": attachment_url,
+                        "buttons": [
+                            {
+                            "type": "postback",
+                            "title": "Yes!",
+                            "payload": "yes",
+                            },
+                            {
+                            "type": "postback",
+                            "title": "No!",
+                            "payload": "no",
+                            }
+                        ],
+                    }]
+                }
+            }
+        }
     }
-    else if(messagePostback) {
-        handlePostback(senderID, messagePostback);
-    }
+    
 }
 
 /*
@@ -245,45 +270,4 @@ function callSendAPI(messageData) {
             console.error("Failed calling Send API", response.statusCode, response.statusMessage, body.error);
         }
     });
-
-    function handlePostback(sender_psid, received_postback) {
-        let response;
-      
-        // Get the payload for the postback
-        let payload = received_postback.payload;
-      
-        // Set the response based on the postback payload
-        if (payload === 'yes') {
-          response = { "text": "Thanks!" }
-        } else if (payload === 'no') {
-          response = { "text": "Oops, try sending another image." }
-        }
-        // Send the message to acknowledge the postback
-        callSendAPI(sender_psid, response);
-      }
-      
-        // Sends response messages via the Send API
-      function callSendAPI(sender_psid, response) {
-        // Construct the message body
-        let request_body = {
-          "recipient": {
-            "id": sender_psid
-          },
-          "message": response
-        }
-      
-        // Send the HTTP request to the Messenger Platform
-        request({
-          "uri": "https://graph.facebook.com/v2.6/me/messages",
-          "qs": { "access_token": PAGE_ACCESS_TOKEN },
-          "method": "POST",
-          "json": request_body
-        }, (err, res, body) => {
-          if (!err) {
-            console.log('message sent!')
-          } else {
-            console.error("Unable to send message:" + err);
-          }
-        });
-    }
 }
